b9a9eaab56469bf9cbfafa31476a64354f580edb
Michael Jimenez <michaeljimenezcontreras1@gmail.com>
2024-01-12 16:56:41 -0500

    Correcciones de comportamiento en API

--- API/API.py
+++ API/API.py
@@ -3,57 +3,64 @@ from flask_cors import CORS
 from pymongo import MongoClient
 from hashlib import sha256
 
-conexion = MongoClient('localhost',27017)
 
-app=Flask(__name__)
-CORS(app)
+with MongoClient('localhost',27017) as conexion:
+    try:
+        app=Flask(__name__)
+        CORS(app)
 
-@app.route('/login',methods=['POST'])
-def login():
-    """Función que verifica la existencia de un usuario dado
+        @app.route('/login',methods=['POST'])
+        def login():
+            """Función que verifica la existencia de un usuario dado
 
-    Returns:
-        json: Contiene nombre y contraseña con hash256
-    """
-    global conexion    
-    db=conexion.SGADB
-
-
-    data=request.get_json()
-    
-    encoder=sha256()
-    encoder.hexdigest()
-    encoder.update(bytes(data['pass'],encoding='latin-1'))
-
-    temp=encoder.hexdigest()
-
-    salida={'access':3, 'accesskey':(data['user'],temp)}
-    usuario=None
-    if (usuario:=db.USUARIOS.find_one({'Codigo':data['user'],'Secret':temp})) is not None:
-        salida['access']=usuario['Tipo-acceso']
-    return jsonify(salida)
-    
-    
-@app.route('/validar',methods=['POST'])
-def validar():
-    global conexion    
-    db=conexion.SGADB
-    data=request.get_json()
-    if (usuario:=db.USUARIOS.find_one({'Codigo':data['user'],'Secret':data['pass']})) is not None:
-        return jsonify({'result':True, 'Tipo-acceso':usuario['Tipo-acceso']})
-    return jsonify({'result':False})
-
-@app.route('/Pruebas')
-def pruebas():
-    global conexion    
-    db=conexion.SGADB
-    encoder=sha256()
-    encoder.hexdigest()
-    encoder.update(bytes('12345',encoding='latin-1'))
-    temp=encoder.hexdigest()
-
-    print({'Codigo':'202020202020','Secret':temp})
-    return db.USUARIOS.find_one({'Codigo':'202020202020','Secret':temp})
+            Returns:
+                json: Contiene nombre y contraseña con hash256
+            """
+            global conexion    
+            db=conexion.SGADB
 
 
-app.run(debug=True)
+            data=request.get_json()
+            
+            encoder=sha256()
+            encoder.hexdigest()
+            encoder.update(bytes(data['pass'],encoding='latin-1'))
+
+            temp=encoder.hexdigest()
+
+            salida={'access':3, 'accesskey':(data['user'],temp)}
+            usuario=None
+            if (usuario:=db.USUARIOS.find_one({'Codigo':data['user'],'Secret':temp})) is not None:
+                salida['access']=usuario['Tipo-acceso']
+            return jsonify(salida)
+            
+            
+        @app.route('/validar',methods=['POST'])
+        def validar():
+            global conexion    
+            db=conexion.SGADB
+            data=request.get_json()
+            if (usuario:=db.USUARIOS.find_one({'Codigo':data['user'],'Secret':data['pass']})) is not None:
+                return jsonify({'result':True, 'Tipo-acceso':usuario['Tipo-acceso']})
+            return jsonify({'result':False})
+
+        @app.route('/datos_estudiante',methods=['POST'])
+        def data():
+            global conexion
+            db=conexion.SGADB
+            data=request.get_json()
+            usuario=db.ESTUDIANTES.find_one({'Codigo':data['user']})
+            proyecto=db.PROYECTOS.find_one({'_id':usuario['Proyectoid']})
+            facultad=db.FACULTADES.find_one({'_id':proyecto['Facultad']})
+            USUARIO=dict(usuario)
+            USUARIO['Proyecto']=proyecto['Nombre']
+            USUARIO['Facultad']=facultad['Nombre']
+            print(USUARIO)
+            return jsonify(USUARIO)
+
+        app.run(debug=True)
+    except:
+        print('Ocurrión un error')
+    finally:
+        print('Cerrando')
+        conexion.close()
